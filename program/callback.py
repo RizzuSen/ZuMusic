7f294e08b09aef056e6bc6f5dea8f6b1b53e0db7
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2021-11-13 16:34:23 +0700

    Add files via upload

---
+++ program/callback.py
@@ -0,0 +1,201 @@
+# Copyright (C) 2021 By VeezMusicProject
+
+from driver.queues import QUEUE
+from pyrogram import Client, filters
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+from config import (
+    ASSISTANT_NAME,
+    BOT_NAME,
+    BOT_USERNAME,
+    GROUP_SUPPORT,
+    OWNER_NAME,
+    UPDATES_CHANNEL,
+)
+
+
+@Client.on_callback_query(filters.regex("cbstart"))
+async def cbstart(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""✨ **Welcome [{query.message.chat.first_name}](tg://user?id={query.message.chat.id}) !**\n
+💭 **[{BOT_NAME}](https://t.me/{BOT_USERNAME}) Membantu Anda memutar musik dan video di grup melalui obrolan video!**
+
+💡 **Cari tau semua Perintah Bot dan cara kerjanya dengan mengklik tombol » 📚command!**
+
+🔖 **Untuk mengetahui cara menggunakan bot ini, silakan klik tombol » ❓ Basic Guide!**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "➕ TAMBAHKAN KE GROUP ➕",
+                        url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+                    )
+                ],
+                [InlineKeyboardButton("❓ Basic Guide", callback_data="cbhowtouse")],
+                [
+                    InlineKeyboardButton("📚 Commands", callback_data="cbcmds"),
+                    InlineKeyboardButton("❤ Donate", url=f"https://t.me/{OWNER_NAME}"),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "👥 Official Group", url=f"https://t.me/{GROUP_SUPPORT}"
+                    ),
+                    InlineKeyboardButton(
+                        "📣 Official Channel", url=f"https://t.me/{UPDATES_CHANNEL}"
+                    ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "🌐 Source Code", url="https://github.com/hdiiofficial/NOTICE"
+                    )
+                ],
+            ]
+        ),
+        disable_web_page_preview=True,
+    )
+
+
+@Client.on_callback_query(filters.regex("cbhowtouse"))
+async def cbguides(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""❓ Panduan Dasar untuk menggunakan bot ini:
+
+1.) Pertama, tambahkan saya ke grup Anda.
+2.) Kemudian, jadikan saya sebagai administrator dan berikan semua izin kecuali Admin Anonim.
+3.) Setelah saya menjadi admin, ketik /reload di grup untuk memperbaharui data admin.
+3.) Tambahkan @{ASSISTANT_NAME} ke grup Anda atau ketik /userbotjoin untuk mengundangnya.
+4.) Nyalakan obrolan video terlebih dahulu sebelum mulai memutar video/musik.
+5.) Terkadang, memuat ulang bot dengan menggunakan perintah /reload dapat membantu Anda memperbaiki beberapa masalah.
+
+Jika userbot tidak bergabung ke video chat, pastikan jika video chat sudah aktif, atau ketik /userbotleave lalu ketik /userbotjoin lagi.
+
+Jika Anda memiliki pertanyaan lanjutan tentang bot ini, Anda dapat menyampaikannya ke saya: @hdiiofficial
+
+Didukung oleh {BOT_NAME} """,
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("🔙 Go Back", callback_data="cbstart")]]
+        ),
+    )
+
+
+@Client.on_callback_query(filters.regex("cbcmds"))
+async def cbcmds(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""✨ **Hello [{query.message.chat.first_name}](tg://user?id={query.message.chat.id}) !**
+
+» **tekan tombol di bawah untuk membaca penjelasan dan melihat daftar perintah yang tersedia!**
+
+ Powered by {BOT_NAME} """,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("👷🏻 Admin Cmd", callback_data="cbadmin"),
+                    InlineKeyboardButton("🧙🏻 Sudo Cmd", callback_data="cbsudo"),
+                ],[
+                    InlineKeyboardButton("📚 Basic Cmd", callback_data="cbbasic")
+                ],[
+                    InlineKeyboardButton("🔙 Go Back", callback_data="cbstart")
+                ],
+            ]
+        ),
+    )
+
+
+@Client.on_callback_query(filters.regex("cbbasic"))
+async def cbbasic(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""🏮 di sini adalah perintah dasar:
+
+» /play (nama lagu/tautan) - putar musik di obrolan video
+» /stream (query/link) - streaming yt live/radio live music
+» /vplay (nama video/tautan) - putar video di obrolan video
+» /vstream - putar video langsung dari yt live/m3u8
+» /playlist - menampilkan daftar putar
+» /video (permintaan) - unduh video dari youtube
+» /song (query) - download lagu dari youtube
+» /lyric (query) - memo lirik lagu
+» /search (query) - cari link video youtube
+
+» /ping - tampilkan status bot ping
+» / uptime - tampilkan status uptime bot
+» /alive - tampilkan info bot hidup (dalam grup)
+
+Didukung oleh {BOT_NAME} """,
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("🔙 Go Back", callback_data="cbcmds")]]
+        ),
+    )
+
+
+@Client.on_callback_query(filters.regex("cbadmin"))
+async def cbadmin(_, query: CallbackQuery):
+    await query.edit_message_text(
+       f"""🏮 berikut adalah perintah admin:
+
+» /jeda - jeda streaming
+» /resume - lanjutkan streaming
+» /skip - beralih ke aliran berikutnya
+» /stop - hentikan streaming
+» /vmute - bisukan bot pengguna di obrolan suara
+» /vunmute - mengaktifkan suara bot pengguna di obrolan suara
+» /volume 1-200 - mengatur volume musik (userbot harus admin)
+» /reload - reload bot dan refresh data admin
+» /userbotjoin - undang userbot untuk bergabung dengan grup
+» /userbotleave - perintahkan userbot keluar dari grup
+
+
+Didukung oleh {BOT_NAME} """,
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("🔙 Go Back", callback_data="cbcmds")]]
+        ),
+    )
+
+@Client.on_callback_query(filters.regex("cbsudo"))
+async def cbsudo(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""🏮 di sini adalah perintah sudo:
+
+» /rmw - bersihkan semua file mentah
+» /rmd - bersihkan semua file yang diunduh
+» /leaveall - perintahkan userbot keluar dari semua grup
+
+Didukung oleh {BOT_NAME} """,
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("🔙 Go Back", callback_data="cbcmds")]]
+        ),
+    )
+
+
+@Client.on_callback_query(filters.regex("cbmenu"))
+async def cbmenu(_, query: CallbackQuery):
+    if query.message.sender_chat:
+        return await query.answer("Anda adalah Admin Anonim !\n\n» kembali ke akun pengguna dari hak admin.")
+    a = await _.get_chat_member(query.message.chat.id, query.from_user.id)
+    if not a.can_manage_voice_chats:
+        return await query.answer("💡 BUTTON INI CUMA UNTUK ADMIN", show_alert=True)
+    chat_id = query.message.chat.id
+    if chat_id in QUEUE:
+          await query.edit_message_text(
+              f"⚙️ **settings of** {query.message.chat.title}\n\n⏸ : pause stream\n▶️ : resume stream\n🔇 : mute userbot\n🔊 : unmute userbot\n⏹ : stop stream",
+              reply_markup=InlineKeyboardMarkup(
+                  [[
+                      InlineKeyboardButton("⏹", callback_data="cbstop"),
+                      InlineKeyboardButton("⏸", callback_data="cbpause"),
+                      InlineKeyboardButton("▶️", callback_data="cbresume"),
+                  ],[
+                      InlineKeyboardButton("🔇", callback_data="cbmute"),
+                      InlineKeyboardButton("🔊", callback_data="cbunmute"),
+                  ],[
+                      InlineKeyboardButton("🗑 Close", callback_data="cls")],
+                  ]
+             ),
+         )
+    else:
+        await query.answer("❌ nothing is currently streaming", show_alert=True)
+
+
+@Client.on_callback_query(filters.regex("cls"))
+async def close(_, query: CallbackQuery):
+    a = await _.get_chat_member(query.message.chat.id, query.from_user.id)
+    if not a.can_manage_voice_chats:
+        return await query.answer("💡 BUTTON INI CUMA UNTUK ADMIN", show_alert=True)
+    await query.message.delete()
---
+++ program/userbotjoin.py
@@ -0,0 +1,85 @@
+import asyncio
+from config import BOT_USERNAME, SUDO_USERS
+from driver.decorators import authorized_users_only, sudo_users_only, errors
+from driver.filters import command, other_filters
+from driver.veez import user as USER
+from pyrogram import Client, filters
+from pyrogram.errors import UserAlreadyParticipant
+
+
+@Client.on_message(
+    command(["userbotjoin", f"userbotjoin@{BOT_USERNAME}"]) & ~filters.private & ~filters.bot
+)
+@authorized_users_only
+@errors
+async def join_group(client, message):
+    chid = message.chat.id
+    try:
+        invitelink = await client.export_chat_invite_link(chid)
+    except BaseException:
+        await message.reply_text(
+            "• **i'm not have permission:**\n\n» ❌ __Add Users__",
+        )
+        return
+
+    try:
+        user = await USER.get_me()
+    except BaseException:
+        user.first_name = "music assistant"
+
+    try:
+        await USER.join_chat(invitelink)
+    except UserAlreadyParticipant:
+        pass
+    except Exception as e:
+        print(e)
+        await message.reply_text(
+            f"🛑Flood wait Error🛑\n\n**Userbot tidak dapat Bergabung dengan Group Anda karena banyaknya permintaan bergabung**"
+            "\n\n**atau tambahkan asisten secara manual ke Group Anda dan coba lagi**,
+        )
+        return
+    await message.reply_text(
+        f"✅ **userbot Berhasil Memasuki Obrolan☕**",
+    )
+
+
+@Client.on_message(command(["userbotleave",
+                            f"leave@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
+@authorized_users_only
+async def leave_one(client, message):
+    try:
+        await USER.send_message(message.chat.id, "✅ userbot berhasil keluar dari obrolan")
+        await USER.leave_chat(message.chat.id)
+    except BaseException:
+        await message.reply_text(
+            "❌ **Userbot tidak dapat meninggalkan Group Anda,mungkin menunggu lama.**\n\n**»atau keluarkan userbot secara manual dari Group Anda**"
+        )
+
+        return
+
+
+@Client.on_message(command(["leaveall", f"leaveall@{BOT_USERNAME}"]))
+@sudo_users_only
+async def leave_all(client, message):
+    if message.from_user.id not in SUDO_USERS:
+        return
+
+    left = 0
+    failed = 0
+    lol = await message.reply("🔄 **userbot** leaving all chats !")
+    async for dialog in USER.iter_dialogs():
+        try:
+            await USER.leave_chat(dialog.chat.id)
+            left += 1
+            await lol.edit(
+                f"Userbot leaving all group...\n\nLeft: {left} chats.\nFailed: {failed} chats."
+            )
+        except BaseException:
+            failed += 1
+            await lol.edit(
+                f"Userbot leaving...\n\nLeft: {left} chats.\nFailed: {failed} chats."
+            )
+        await asyncio.sleep(0.7)
+    await client.send_message(
+        message.chat.id, f"✅ Left from: {left} chats.\n❌ Failed in: {failed} chats."
+    )

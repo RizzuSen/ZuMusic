251707aafd1ea90fe5b397e482079e3f00b87078
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2021-11-13 18:34:45 +0700

    Update music.py

--- program/music.py
+++ program/music.py
@@ -271,7 +271,7 @@ async def stream(c: Client, m: Message):
                 InlineKeyboardButton(text="• MENU", callback_data="cbmenu"),
                 InlineKeyboardButton(text="• CLOSE", callback_data="cls"),
             ],[
-                InlineKeyboardButton(text="✨ CHANNEL, url=f"https://t.me/{UPDATES_CHANNEL}"),
+                InlineKeyboardButton(text="✨ CHANNEL", url=f"https://t.me/{UPDATES_CHANNEL}"),
             ]
         ]
     )

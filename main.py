2768da8dc3fdfb0d47f7cad7eef41d1f7e4a90c3
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2021-11-13 16:55:17 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import asyncio
+from pytgcalls import idle
+from driver.veez import call_py, bot
+
+async def mulai_bot():
+    print("[VEEZ]: STARTING BOT CLIENT")
+    await bot.start()
+    print("[VEEZ]: STARTING PYTGCALLS CLIENT")
+    await call_py.start()
+    await idle()
+    print("[VEEZ]: STOPPING BOT & USERBOT")
+    await bot.stop()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(mulai_bot())

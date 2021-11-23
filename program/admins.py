5ef7a02561271f54c2b2283341b7070b829ea299
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2021-11-23 22:03:35 +0700

    Update admins.py

--- program/admins.py
+++ program/admins.py
@@ -46,10 +46,10 @@ async def skip(client, m: Message):
         [
             [
                 InlineKeyboardButton(
-                    text="• Mᴇɴᴜ", callback_data="cbmenu"
+                    text="• MENU", callback_data="cbmenu"
                 ),
                 InlineKeyboardButton(
-                    text="• Cʟᴏsᴇ", callback_data="cls"
+                    text="• CLOSE", callback_data="cls"
                 ),
             ]
         ]

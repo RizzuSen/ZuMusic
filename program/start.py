4ab586a8ebf1307d2b7b7376434b0f9f9b2d9c59
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2021-11-25 07:24:27 +0700

    Update start.py

--- program/start.py
+++ program/start.py
@@ -11,7 +11,6 @@ from config import (
     OWNER_NAME,
     UPDATES_CHANNEL,
 )
-from teks import START_BOT, UNMUTE
 from program import __version__
 from driver.filters import command, other_filters
 from pyrogram import Client, filters

89baa1373eeb725eb1b89a1f3b0c4d6b1b6dab99
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2021-12-18 13:34:58 +0100

    Update music.py

--- program/music.py
+++ program/music.py
@@ -46,7 +46,7 @@ async def ytdl(link):
         return 0, stderr.decode()
 
 
-@Client.on_message(command(["taik", f"taik@{BOT_USERNAME}"]) & other_filters)
+@Client.on_message(command(["play", f"play@{BOT_USERNAME}"]) & other_filters)
 async def play(c: Client, m: Message):
     replied = m.reply_to_message
     chat_id = m.chat.id
